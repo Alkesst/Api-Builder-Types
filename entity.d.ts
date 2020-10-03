7ef45fa13fa1c7a6650609864572794b0bbfec65
Aleks <alec.augal@gmail.com>
2020-10-03 19:42:38 +0200

    Added entity config types. (#2)
    * Added entity config types. Work in progress.
    
    * Added group to the entities
    
    * Added related entities of Entity.#]
    
    * Solved errors with typings
    
    * Solved errors with typings
    
    * Solved typing errors

---
+++ entity.d.ts
@@ -0,0 +1,14 @@
+import { Guid } from 'guid-typescript';
+import { Attribute } from './attribute';
+import { Coordinates } from './coordinates';
+import { Relationship } from "./relationship";
+import { Constraint } from "./constraint";
+
+export interface Entity {
+    Identifier: Guid;
+    Name: string;
+    Relationships: Relationship[];
+    Attributes: Attribute[];
+    Coordinates: Coordinates;
+    Constraints: Constraint[];
+}
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
 import { Attribute, AttributeType } from './attribute';
+import { Entity } from './entity';
+import { Coordinates } from './coordinates';
+import { Group } from "./group";
+import { Constraint } from "./constraint";
 
 export {
-    Attribute, AttributeType
+    Attribute,
+    AttributeType,
+    Entity,
+    Coordinates,
+    Group,
+    Constraint
 }
