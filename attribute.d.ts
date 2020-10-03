4feeba670ab18f6dea41c08663416484bdce81e8
Aleks <alec.augal@gmail.com>
2020-10-03 19:12:05 +0200

    Added library for Guid types. Finished Attribute Config Structure (#1)
    * Added a new dependency to the project. Finished Attribute config structure
    
    * Added example of configuration
    
    * Updated attribute config structure
    
    * Rollback readme.md

--- attribute.d.ts
+++ attribute.d.ts
@@ -1,4 +1,14 @@
+import { Guid } from 'guid-typescript';
+
 export interface Attribute {
-    Type: 'string' | 'boolean' | 'number' | 'date';
-    Value: string;
+    Identifier: Guid;
+    Type: AttributeType;
+    Value: string | null;
+    IsNullable: boolean;
+    DefaultValue: string | null;
+    Precision: number | null;
+}
+
+export enum AttributeType {
+    string, date, bool, numeric
 }
