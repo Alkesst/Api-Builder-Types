b7bc738eceb93b9ba7db1ec22a363bbfe7a37db3
Samuel Jurado <samueljurado12@gmail.com>
2022-02-02 19:36:29 +0100

    Naming things as they should would be a huge advance

--- constraint.js
+++ constraint.js
@@ -1,8 +1,8 @@
-const AttributeType  = Object.freeze({
+const ConstraintType  = Object.freeze({
     Unique: 1,
     Index: 2,
     1: 'Unique',
     2: 'Index',
 });
 
-module.exports = { AttributeType };
+module.exports = { ConstraintType };
