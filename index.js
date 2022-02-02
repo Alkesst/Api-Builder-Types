994c192e1d746d0d4a17601b67f7fd96a4a38ce6
Samuel Jurado <samueljurado12@gmail.com>
2022-02-02 19:40:22 +0100

    tusmu ertos

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-module.exports = { ProjectType: require('./project-type').ProjectType, AttributeType : require('./attribute').AttributeType,
-    ConstraintType : require('./constraint').ConstraintType };
+module.exports = { ProjectType: require('./project-type').ProjectType, AttributeType : require('./attribute').AttributeType, ConstraintType : require('./constraint').ConstraintType };
