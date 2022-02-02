c1b87dba9f7d5e45481fd4b0dbb8cb87917c4dd9
Samuel Jurado <samueljurado12@gmail.com>
2022-02-02 19:24:59 +0100

    Added ConstraintType to export

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = { ProjectType: require('./project-type').ProjectType, AttributeType : require('./attribute').AttributeType };
+module.exports = { ProjectType: require('./project-type').ProjectType, AttributeType : require('./attribute').AttributeType, ConstraintType : require('./constraint').ConstraintType };
